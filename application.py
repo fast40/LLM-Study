424a491ba37764cc96a6847a9867c800ee28d6a8
fast40 <eclfast@gmail.com>
2023-05-01 20:26:09 -0700

    Initial commit

---
+++ application.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, send_file
+import os
+
+application = Flask(__name__)
+
+
+@application.route('/')
+def index():
+	if 'quantity' in request.args:
+		return os.environ('RDS_HOSTNAME')
+	else:
+		return 'LLM-Survey V1.0'
+		
+
+@application.route('/robots.txt')
+def robots():
+	return send_file('static/robots.txt')
+
+
+if __name__ == '__main__':
+	application.run(host='0.0.0.0', port=3000, debug=True)
