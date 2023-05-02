d9ace9abc902820fa80b145b214ef32cca1c30c5
fast40 <eclfast@gmail.com>
2023-05-01 20:52:37 -0700

    Print errors

--- application.py
+++ application.py
@@ -16,7 +16,11 @@ session = boto3.Session(profile_name='default')
 client = session.client('rds')
 
 token = client.generate_db_auth_token(DBHostname=RDS_HOSTNAME, Port=RDS_PORT, DBUsername=RDS_USERNAME, Region='us-west-1')
-connection =  pymysql.connect(host=RDS_HOSTNAME, user=RDS_USERNAME, passwd=token, port=RDS_PORT, database=RDS_DB_NAME, ssl_ca='SSLCERTIFICATE')
+print(token)
+try:
+	connection =  pymysql.connect(host=RDS_HOSTNAME, user=RDS_USERNAME, passwd=token, port=RDS_PORT, database=RDS_DB_NAME, ssl_ca='SSLCERTIFICATE')
+except Exception as e:
+	print(e)
 
 @application.route('/')
 def index():
