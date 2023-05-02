67a6348527b85c68dd4d6eebba18f4217c990fb3
fast40 <eclfast@gmail.com>
2023-05-01 20:49:04 -0700

    Add SQL functionality

--- application.py
+++ application.py
@@ -1,13 +1,27 @@
 from flask import Flask, request, send_file
+from utils import get_questions
+import pymysql
+import boto3
 import os
 
 application = Flask(__name__)
 
+RDS_HOSTNAME = os.environ.get('RDS_HOSTNAME')
+RDS_PORT = os.environ.get('RDS_PORT')
+RDS_DB_NAME = os.environ.get('RDS_DB_NAME')
+RDS_USERNAME = os.environ.get('RDS_USERNAME')
+RDS_PASSWORD = os.environ.get('RDS_PASSWORD')
+
+session = boto3.Session(profile_name='default')
+client = session.client('rds')
+
+token = client.generate_db_auth_token(DBHostname=RDS_HOSTNAME, Port=RDS_PORT, DBUsername=RDS_USERNAME, Region='us-west-1')
+connection =  pymysql.connect(host=RDS_HOSTNAME, user=RDS_USERNAME, passwd=token, port=RDS_PORT, database=RDS_DB_NAME, ssl_ca='SSLCERTIFICATE')
 
 @application.route('/')
 def index():
 	if 'quantity' in request.args:
-		return os.environ.get('RDS_HOSTNAME')
+		return get_questions(connection, request.args['quantity'])
 	else:
 		return 'LLM-Survey V1.0'
 		
---
+++ utils.py
@@ -0,0 +1,23 @@
+SOURCES = ('source1', 'source2', 'source3')
+
+def get_questions(connection, cursor, quantity):
+	questions = {}
+
+	for source in SOURCES:
+		with connection.cursor() as cursor:
+			cursor.execute(f'SELECT id, question, answer FROM questions WHERE source = "{source}" AND views < 1 LIMIT {quantity}')
+
+		query_results = cursor.fetchall()
+		questions[source] = query_results
+
+		id_values = ', '.join([str(query_result[0]) for query_result in query_results])
+		
+		if id_values == '':
+			continue
+
+		with connection.cursor() as cursor:
+			cursor.execute(f'UPDATE questions SET views = views + 1 WHERE id IN ({id_values})')
+
+		connection.commit()
+	
+	return questions
